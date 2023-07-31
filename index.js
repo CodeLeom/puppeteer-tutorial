3784a3500da035914fdccb79af1016d96c6710f8
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-07-31 16:54:49 +0100

    feat: update code implementation

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+import puppeteer from "puppeteer";
 
 	(async () => {
   // Launch a headless browser
@@ -10,19 +10,19 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
 
   // Navigate to the login page
-  await page.goto('http://127.0.0.1:5500/index.html'); // Replace 'http://127.0.0.1:5500/index.html' with the actual path to your HTML file.
+  await page.goto('https://warehouse-theme-metal.myshopify.com/account/login'); // Replace 'https://warehouse-theme-metal.myshopify.com/account/login' with the actual path to your HTML file.
 
   // Wait for the form to be visible on the page
-  await page.waitForSelector('#login-form');
+  await page.waitForSelector('#customer_login');
 
   // Fill in the form inputs
-  await page.type('#uname', 'demo_username');
-  await page.type('#pwd', 'demo_password');
+  await page.type('#customer\\[email\\]', 'demo_username');
+  await page.type('#customer\\[password\\]', 'demo_password');
 
   // Submit the form by clicking the login button
   await Promise.all([
     page.waitForNavigation(), // Wait for navigation to complete
-    page.click('#login'),
+    page.click('.form__submit'),
   ]);
 
   // Verify successful login by checking the URL or page content
